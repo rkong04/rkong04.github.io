398583bd2db1372f6c38e5d25243cfd560d76f4e
Raymond <raymondtykong@gmail.com>
2024-06-27 13:50:38 -0700

    finished website with links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
-    const menu = document.querySelector(".menu");
+    const menuLinks = document.querySelector(".menu-links");
     const icon = document.querySelector(".icon");
-    menu.classList.toggle("open")
+    menuLinks.classList.toggle("open")
     icon.classList.toggle("open")
 }
